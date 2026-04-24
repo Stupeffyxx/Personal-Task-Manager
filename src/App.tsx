5fb7078b9482e126657bf9bc33aaef357515187a
BUDI SULISTIANTO <budisulistianto17@gmail.com>
2026-04-24 22:46:38 +0800

    configure

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  // how to use react
+  // how to use tailwind
+  return (
+    <>
+      <section className="bg-[#e8e7e1] h-screen w-full flex">
+
+      </section>
+    </>
+  )
+}
+
+export default App
