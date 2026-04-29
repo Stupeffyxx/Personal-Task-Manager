c56cccd7dd746763322b7ddce5c33b4cfa6f7e2b
BUDI SULISTIANTO <budisulistianto17@gmail.com>
2026-04-29 21:46:50 +0800

    comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
+
+
 function App() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100 overflow-hidden" >
-      
-      <div className="bg-white p-6 rounded-lg shadow-md w-96 h-150">
+     
+      <div className="bg-white p-6 rounded-lg shadow-md w-96 h-150 overflow-hidden">
         <h1 className="text-2xl font-bold ">
           My Task
         </h1>
-        
+
 
         {/* date */}
         <p className="text-gray-500">Sunday, April 19</p>
 
- {/* INPUT */}
+        {/* INPUT */}
         <div className="flex gap-2 mb-4">
           <input
             type="text"
@@ -23,126 +25,104 @@ function App() {
             +
           </button>
         </div>
-<div className="flex items-center my-4">
-  <div className="flex-1 h-px bg-gray-300"></div>
-  
-  <span className="px-2 text-gray-500 text-sm">
-   Input Field + Add button
-  </span>
-  
-  <div className="flex-1 h-px bg-gray-300"></div>
-</div>
 
-<div className="bg-gray-100 p-2 rounded flex justify-between">
 
-  <button className="flex-1 py-2 rounded bg-blue-500 text-white">
-    All
-  </button>
+        <div className="bg-gray-100 p-2 rounded flex justify-between my-4">
 
-  <button className="flex-1 py-2 rounded text-gray-600">
-    Active
-  </button>
+          <button className="flex-1 py-2 rounded bg-blue-500 text-white">
+            All
+          </button>
 
-  <button className="flex-1 py-2 rounded text-gray-600">
-    Done
-  </button>
+          <button className="flex-1 py-2 rounded text-gray-600">
+            Active
+          </button>
 
-</div>
-<div className="flex items-center my-4">
-  <div className="flex-1 h-px bg-gray-300"></div>
-  
-  <span className="px-2 text-gray-500 text-sm">
-    Filter Buttons
-  </span>
-  
-  <div className="flex-1 h-px bg-gray-300"></div>
-</div>
+          <button className="flex-1 py-2 rounded text-gray-600">
+            Done
+          </button>
 
-<div className="space-y-2">
+        </div>
 
-  {/* ITEM */}
-  <div className="flex items-center justify-between bg-gray-100 p-3 rounded">
-    
-    {/* KIRI: checkbox + text */}
-    <div className="flex items-center gap-2">
-      <input type="checkbox" className="w-4 h-4" />
-      <span>Belajar React</span>
-    </div>
+        <div className="space-y-2">
 
-    {/* KANAN: delete */}
-    <button className="text-red-500 font-bold">
-      X
-    </button>
+          {/* ITEM */}
+          <div className="flex items-center justify-between bg-gray-100 p-3 rounded">
 
-  </div>
+            {/* KIRI: checkbox + text */}
+            <div className="flex items-center gap-2">
+              <input type="checkbox" className="w-4 h-4" />
+              <span>Belajar React</span>
+            </div>
 
-  {/* ITEM */}
-  <div className="flex items-center justify-between bg-gray-100 p-3 rounded">
-    <div className="flex items-center gap-2">
-      <input type="checkbox" className="w-4 h-4" />
-      <span>Kerjain tugas</span>
-    </div>
+            {/* KANAN: delete */}
+            <button className="text-red-500 font-bold">
+              X
+            </button>
 
-    <button className="text-red-500 font-bold">
-      X
-    </button>
-  </div>
+          </div>
 
-</div>
+          {/* ITEM */}
+          <div className="flex items-center justify-between bg-gray-100 p-3 rounded">
+            <div className="flex items-center gap-2">
+              <input type="checkbox" className="w-4 h-4" />
+              <span>Kerjain tugas</span>
+            </div>
 
-<div className="space-y-2">
+            <button className="text-red-500 font-bold">
+              X
+            </button>
+          </div>
 
-  {/* ITEM */}
-  <div className="flex items-center justify-between bg-gray-100 p-3 rounded">
-    
-    {/* KIRI: checkbox + text */}
-    <div className="flex items-center gap-2">
-      <input type="checkbox" className="w-4 h-4" />
-      <span>Belajar React</span>
-    </div>
 
-    {/* KANAN: delete */}
-    <button className="text-red-500 font-bold">
-      X
-    </button>
 
-  </div>
+          {/* ITEM */}
+          <div className="flex items-center justify-between bg-gray-100 p-3 rounded">
 
-  {/* ITEM */}
-  <div className="flex items-center justify-between bg-gray-100 p-3 rounded">
-    <div className="flex items-center gap-2">
-      <input type="checkbox" className="w-4 h-4" />
-      <span>Kerjain tugas</span>
-    </div>
+            {/* KIRI: checkbox + text */}
+            <div className="flex items-center gap-2">
+              <input type="checkbox" className="w-4 h-4" />
+              <span>Belajar React</span>
+            </div>
 
-    <button className="text-red-500 font-bold">
-      X
-    </button>
-  </div>
+            {/* KANAN: delete */}
+            <button className="text-red-500 font-bold">
+              X
+            </button>
 
-  <div className="flex items-center justify-between bg-gray-100 p-3 rounded">
-    <div className="flex items-center gap-2">
-      <input type="checkbox" className="w-4 h-4" />
-      <span>Kerjain tugas</span>
-    </div>
+          </div>
 
-    <button className="text-red-500 font-bold">
-      X
-    </button>
-  </div>
-  
-  <div className="flex items-center my-4">
-  <div className="flex-1 h-px bg-gray-300"></div>
-  
-  <span className="px-2 text-gray-500 text-sm">
-    checkbox - task text - X delete
-  </span>
-  
-  <div className="flex-1 h-px bg-gray-300"></div>
-</div>
+          {/* ITEM */}
+          <div className="flex items-center justify-between bg-gray-100 p-3 rounded">
+            <div className="flex items-center gap-2">
+              <input type="checkbox" className="w-4 h-4" />
+              <span>Kerjain tugas</span>
+            </div>
 
- <p className="text-gray-500">Sunday, April 19</p>
-</div>
+            <button className="text-red-500 font-bold">
+              X
+            </button>
+          </div>
+
+          <div className="flex items-center justify-between bg-gray-100 p-3 rounded">
+            <div className="flex items-center gap-2">
+              <input type="checkbox" className="w-4 h-4" />
+              <span>Kerjain tugas</span>
+            </div>
+
+            <button className="text-red-500 font-bold">
+              X
+            </button>
+          </div>
+
+
+          <div className="flex justify-between items-center my-4">
+            <p className="text-gray-500">3 Task Left</p>
+
+            <button className="py-2 px-6 rounded bg-blue-500 text-white w-fit ">
+              Clear Completed
+            </button>
+          </div>
+        </div>
 
 
 
@@ -151,7 +131,7 @@ function App() {
 
     </div>
 
-    
+
   )
 }
 
